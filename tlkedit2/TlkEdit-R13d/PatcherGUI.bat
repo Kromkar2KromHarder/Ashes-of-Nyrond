a1a39db9e1d77b830f56cf4cd3c68cd27b6765f8
rainbownights <bonkle333@gmail.com>
2026-05-09 20:35:26 -0400

    tlkedit2 accidentally added, removed and ignored

--- tlkedit2/TlkEdit-R13d/PatcherGUI.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-javaw.exe -cp tlkedit.jar org.jl.nwn.patcher.PatcherGUI
--- tlkedit2/TlkEdit-R13d/patcherGui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -z $TLKEDIT_HOME ]; then
-  echo "TLKEDIT_HOME not set, using current directory : " `pwd`
-  TLKEDIT_HOME=. ;
-fi
-java -cp "$TLKEDIT_HOME/tlkedit.jar" org.jl.nwn.patcher.PatcherGUI
